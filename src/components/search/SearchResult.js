f925c976444575b4b35f4a7de2b661bb545dbc9c
Maël <chanon.mael@gmail.com>
2024-02-06 19:42:20 +0100

    changed search result

--- src/components/search/SearchResult.js
+++ src/components/search/SearchResult.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet, useWindowDimensions, Platform} from 'react-native';
+import React, {useState} from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    useWindowDimensions,
+    Platform,
+    Pressable,
+    TouchableHighlight
+} from 'react-native';
 import commonStyles from "../../style/commonStyle";
 import {Colors} from "../../style/color";
+import platform from "react-native-web/src/exports/Platform";
 
-function SearchResult({imageURL, title, subtitle, rounded}) {
+function SearchResult({imageURL, title, subtitle, rounded, onPress}) {
     const baseImageURL = "https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/egg-3442-e1f6463624338504cd021bf23aef8441@1x.jpg"
     const {height, width} = useWindowDimensions()
+    const [isHovered,setIsHovered] = useState(false)
+    const handlePressIn = () => {
+        setIsHovered(true);
+
+    };
+    const handlePressOut = () => {
+        setIsHovered(Platform.OS === "web" && isHovered);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
     const styles = StyleSheet.create({
         diplayContainer: {
             display: "flex",
             flexDirection: "row",
             flex: 1,
+            gap: 10,
         },
         searchResultContainer: {
-            width: "100%",
-            gap: 10,
+            width: Platform.OS === 'web'? "93%": "100%",
+            backgroundColor: isHovered ? Colors.Jet : "transparent",
+            borderRadius: 5,
         },
         infoContainer: {
             alignItems: "center",
@@ -22,6 +46,9 @@ function SearchResult({imageURL, title, subtitle, rounded}) {
             gap: 15
         },
         resultImage: {
+            marginTop: 5,
+            marginBottom: 5,
+            marginLeft: 5,
             height: 50,
             width: 50
         },
@@ -53,52 +80,60 @@ function SearchResult({imageURL, title, subtitle, rounded}) {
 
     const imageStyle = rounded ? styles.imageRound : styles.imageSquare
     return (
-        <View
-            style={[styles.diplayContainer,styles.searchResultContainer]}
+        <TouchableHighlight
+            onPressIn={() => {}} onPressOut={handlePressOut}
+            onMouseEnter={handlePressIn} onMouseLeave={handleMouseLeave}
+            underlayColor={Colors.Jet}
+            style={[styles.searchResultContainer]}
         >
-            <Image
-                source={{
-                uri: imageURL || baseImageURL
-                }}
-                style={[styles.resultImage,imageStyle]}
-            />
             <View
-                style={[styles.diplayContainer, styles.infoContainer]}
+                style={[styles.diplayContainer]}
             >
-                <Text
-                    numberOfLines={3}
-                    ellipsizeMode="tail"
-                    style={[commonStyles.text,styles.limitSize]}
-                >
-                    {title}
-                </Text>
-                {subtitle && width > 250 ?
-                    <Text
-                        numberOfLines={1}
-                        ellipsizeMode="tail"
-                        style={[styles.subtitleContainer,styles.diplayContainer]}
-                    >
-                        <Text>
-                            <Text
 
-                                style={[styles.dot,styles.diplayContainer]}
-                            >
-                                {'\u2B24' + "     "}
-                            </Text>
-                        </Text>
+                <Image
+                    source={{
+                        uri: imageURL || baseImageURL
+                    }}
+                    style={[styles.resultImage,imageStyle]}
+                />
+                <View
+                    style={[styles.diplayContainer, styles.infoContainer]}
+                >
+                    <Text
+                        numberOfLines={3}
+                        ellipsizeMode="tail"
+                        style={[commonStyles.text,styles.limitSize]}
+                    >
+                        {title}
+                    </Text>
+                    {subtitle && width > 250 ?
                         <Text
                             numberOfLines={1}
                             ellipsizeMode="tail"
-                            style={styles.subtitles}
+                            style={[styles.subtitleContainer,styles.diplayContainer]}
                         >
-                            {subtitle}
+                            <Text>
+                                <Text
+
+                                    style={[styles.dot,styles.diplayContainer]}
+                                >
+                                    {'\u2B24' + "     "}
+                                </Text>
+                            </Text>
+                            <Text
+                                numberOfLines={1}
+                                ellipsizeMode="tail"
+                                style={styles.subtitles}
+                            >
+                                {subtitle}
+                            </Text>
                         </Text>
-                    </Text>
-                     : null}
+                        : null}
+
+                </View>
 
             </View>
-
-        </View>
+        </TouchableHighlight>
     );
 }
 
