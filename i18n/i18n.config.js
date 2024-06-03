8872801a3a767d5391cfe29c7d084b00ae564bba
Maël <chanon.mael@gmail.com>
2024-06-03 23:00:11 +0200

    i18n correction

--- i18n/i18n.config.js
+++ i18n/i18n.config.js
@@ -1,21 +1,38 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import fr from "./fr/fr.json";
 import en from "./en/en.json";
 const resources = {
   en: { translation: en },
   fr: { translation: fr },
 };
+console.log(i18next.language);
 i18next
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    whitelist: ["fr", "en"],
     debug: true,
     compatibilityJSON: "v3", //To make it work for Android devices, add this line.
     resources,
-    lng: "fr", // default language to use.
-    // if you're using a language detector, do not define the lng option
-    interpolation: {
-      escapeValue: false,
+    detection: {
+      order: [
+        "querystring",
+        "cookie",
+        "localStorage",
+        "navigator",
+        "htmlTag",
+        "path",
+        "subdomain",
+      ],
+      caches: ["localStorage", "cookie"], // Where to store the detected language
+      lookupQuerystring: "lng",
+      lookupCookie: "i18next",
+      lookupLocalStorage: "i18nextLng",
+      lookupFromPathIndex: 0,
+      lookupFromSubdomainIndex: 0,
+      checkWhitelist: true,
     },
   });
 
